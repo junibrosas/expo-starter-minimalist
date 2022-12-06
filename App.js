12ac9f63c1b946f70923075d49e11f47f27843a7
Juni Brosas <junibrosas@gmail.com>
2022-12-06 13:58:31 +0800

    feat: update expo-publish yml

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Please open up App.js to start working on your app!</Text>
+      <Text>Please, please open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
